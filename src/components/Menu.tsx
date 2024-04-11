cc872dfa8c3cf8ae44d6f994d166838221f1d328
Shahar Mayzel <shahar.mayzel@cyesec.com>
2024-04-12 01:53:55 +0300

    nav bar implement

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,45 @@
-import { Box } from '@mui/material'
+import {  withStyles } from '@material-ui/core/styles'
+import {Link} from 'react-router-dom';
+const styles = () => ({
+ main: {
+    padding: '10px',
+    backgroundColor: '#FFCC31',
+    height: '100%',
+    color: 'black'
+ },
+ list: {
+    listStyle: 'none',
+    textAlign: 'right',
+    margin: '0px',
+    padding: '0px',
+    display: 'flex',
+    flexDirection: 'column'
+ },
+ item : {
+    color: 'black',
+    margin: '10px',
+    textDecoration: 'none',
+    fontSize: '1.5rem',
+    fontWeight: '600'
+ }
+})
+type Classes = Record<keyof ReturnType<typeof styles>, string>
 
-export const Menu = () => <Box>TODO</Box>
+// @ts-ignore
+export const Menu = withStyles(styles)(({ classes }: { classes: Classes }) => {
+    
+   return( 
+        <div className={classes.main}>
+            <div className={classes.list}>
+      
+                <Link to="/info" className={classes.item}>מהי הורות משותפת</Link>
+                <Link to="/contracts" className={classes.item}>הסכם הורות משותפת לדוגמה</Link>
+                <Link to="/groups" className={classes.item}>קבוצות פייסבוק</Link>
+                <Link to="/providers" className={classes.item}>נותני שירותים</Link>
+                <Link to="/community" className={classes.item}>אשמח ללוות תהליך</Link>
+            </div>
+        </div>
+   )
+   
+}
+)
