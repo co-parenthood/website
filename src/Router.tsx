c4bf1ac583145958009b3cdc17fee1319e6ac5e9
Shahar Mayzel <shahar.mayzel@cyesec.com>
2024-04-11 23:53:27 +0300

    contracts page

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,6 +11,7 @@ import {
 import { Community } from './components/Community.tsx'
 import { Info } from './components/Info.tsx'
 import { Providers } from './components/Providers.tsx'
+import { Contracts } from './components/Contracts.tsx'
 
 export const Router = () => (
     <BrowserRouter>
@@ -20,7 +21,7 @@ export const Router = () => (
                 <Route path={GroupsRoute} element={<div>ok</div>} />
                 <Route path={InfoRoute} element={<Info/>} />
                 <Route path={ProvidersRoute} element={<Providers/>} />
-                <Route path={ContractsRoute} element={<div>ok</div>} />
+                <Route path={ContractsRoute} element={<Contracts/>} />
                 <Route path={CommunityRoute} element={<Community />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
---
+++ src/components/Contracts.tsx
@@ -0,0 +1,112 @@
+import { withStyles } from '@material-ui/core/styles'
+import { useLocation } from 'react-router-dom'
+import { CommunityRoute } from '../routes.ts'
+
+const styles = () => ({
+    mainContainer: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'black',
+        color: 'white',
+        fontSize: '1rem'
+    },
+    content: {
+        width: 'calc(100% - 220px)',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '30px',
+        height: '100%'
+
+    },
+    header: {
+        height: '100px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '2rem',
+    },
+    hero: {
+        backgroundColor: '#178c98',
+        height: '500px',
+        display: 'flex',
+        justifyContent: 'space-around',
+        padding: '40px'
+     
+    },
+    title: {
+        fontSize: '1.5rem'
+    },
+    contract: {
+        cursor: 'pointer',
+        borderRadius: '10px',
+        width: '300px',
+        backgroundColor: '#ebebeb3b',
+        transition: 'transform 0.2s ease, background-color 0.2s ease', 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        '&:hover': { 
+            transform: 'scale(1.05)', 
+            backgroundColor: '#62eadb80' 
+        }
+    },
+    contractImg: {
+        borderRadius: '10px'
+    },
+    descript: {
+        marginTop: '10px',
+        width: '250px'
+    },
+    downloadable : {
+        width:'300px',
+        height:'420px',
+        position: 'absolute'
+    }
+
+    
+    
+  
+  
+})
+type Classes = Record<keyof ReturnType<typeof styles>, string>
+
+export const Contracts = withStyles(styles)(({
+    classes,
+}: {
+    classes: Classes
+}) => {
+    const { pathname } = useLocation()
+    if (pathname.endsWith(CommunityRoute)) {
+        return
+    }
+  
+    return (
+       <div className={classes.mainContainer}>
+        <div className={classes.header}>הסכם הורות משותפת לדוגמה</div>
+        <div className={classes.hero}>
+            <div className={classes.contract} >
+                <a href="/pdfs/contracts1.pdf" className={classes.downloadable}></a>
+                <img  src="/contract1.png"   width='270px' className={classes.contractImg} />
+                <div className={classes.descript}>
+                   הסכם  הסכם הסכם מילים מילים בלה בלה בלה שלום אני שחר מילים מילים בלה בלה בלה שלום אני שחר
+                </div>
+            </div>
+            <div className={classes.contract}>
+                <a href="/pdfs/contracts1.pdf" className={classes.downloadable}></a>
+                <img src="/contract2.png"  width='270px' className={classes.contractImg}/>
+                <div className={classes.descript}>
+                הסכם הסכם מילים מילים בלה בלה בלה שלום אני שחר הסכם הסכם מילים מילים בלה בלה בלה שלום אני שחר
+                </div>
+            </div>
+            <div className={classes.contract}>
+                <a href="/pdfs/contracts1.pdf" className={classes.downloadable}></a>
+                <img src="/contract3.png"  width='270px' className={classes.contractImg}/>
+                <div className={classes.descript}>
+                הסכם הסכם מילים מילים בלה בלה בלה שלום אני שחר הסכם הסכם מילים מילים בלה בלה בלה שלום אני שחר
+                </div>
+            </div>
+        </div>
+       </div>
+    )
+})
