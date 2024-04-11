31e0daede293d36c5ce36a82f37661f9f47a5cfd
shaked.katzir <shaked.katzir@cyesec.com>
2024-04-11 19:29:22 +0300

    adding google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Router } from './Router.tsx'
+import ReactGA from 'react-ga4'; ReactGA.initialize('G-H4D2PKWP6X');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
