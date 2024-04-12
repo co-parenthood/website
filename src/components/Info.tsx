b2d7f735a595587502a0163e4228b74f58ccab8f
ReallyLiri <reallyliri@gmail.com>
2024-04-12 08:10:06 +0300

    info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,6 +1,7 @@
 import { Theme, withStyles } from '@material-ui/core/styles'
 import { useLocation } from 'react-router-dom'
 import { JoinRoute } from '../routes.ts'
+import { Contents } from '../contents.ts'
 
 const styles = (theme: Theme) => ({
     mainContainer: {
@@ -53,48 +54,29 @@ export const Info = withStyles(styles)(({ classes }: { classes: Classes }) => {
             <div className={classes.header}>מהי הורות משותפת</div>
             <div className={classes.hero}>
                 <div className={classes.image}>
-                    <img src="/parents.png" width="400px" height="400px"></img>
+                    <img
+                        alt="info"
+                        src={Contents.info.image}
+                        height="400px"
+                    ></img>
                 </div>
                 <div className={classes.info}>
-                    <div className={classes.title}>
-                        הורות משותפת (באנגלית: Shared Parenting) היא שם כולל
-                        למודלים של הורות המהוות חלופה לנישואים ומבוססות על שלושה
-                        עקרונות:
-                    </div>
+                    <div className={classes.title}>{Contents.info.title} </div>
                     <ul>
-                        <li>
-                            זכותו של הילד לקשר משמעותי עם כל הוריו גם אם אינם
-                            נשואים זה לזה.
-                        </li>
-                        <li>
-                            נשיאה משותפת באחריות הורית וכלכלית לילד, בכך שכל
-                            הוריו יגדלו אותו יחדיו, גם אם אינם רשומים כנשואים,
-                            וגם אם אין ביניהם קשר רומנטי.
-                        </li>
-                        <li>זכותו של כל הורה בנפרד לקשר משמעותי עם הילד.</li>
+                        {Contents.info.principals.map((p) => (
+                            <li key={p}>{p}</li>
+                        ))}
                     </ul>
-                    <div>
-                        יישום נפוץ של מודל זה כולל נשים וגברים שהחליטו להביא יחד
-                        לעולם ילד, או ילדים משותפים, מבלי לחיות בזוגיות או לקיים
-                        משק בית משותף. ההורים יכולים להיות הטרוסקסואלים או
-                        חד-מיניים, עם בני זוג או בלעדיהם. תהליך ההתעברות מתבצע
-                        באמצעות הפריה מלאכותית בבית חולים, באופן עצמי, במרפאת
-                        פוריות, או ביחסי מין.
-                    </div>
-                    <div>
-                        לשם כך, לרוב יערכו ביניהם ההורים הסכם הורות משותפת קודם
-                        לתהליך. ההסכם יכול להתקיים בין הורים שיש ביניהם אמון
-                        ומערכת יחסים מינימלית לגידול משותף של ילד, גם אם הם
-                        רשומים פורמלית כרווקים, או גרושים. ההסכם, שנחתם לרוב
-                        טרום הולדת הילד, כולל הסדרה והתייחסות לתחומים הקשורים
-                        להריון, טיפולי פוריות, חלוקת אחריות כלכלית, חלוקת ימי
-                        טיפול בילד וסוגיות כמו חינוך, הסעות ועוד.
-                    </div>
-                    <div className={classes.wiki}>
-                        מתוך ויקיפדיה (הזכויות שמורות)
-                    </div>
-                    <a href="https://he.wikipedia.org/wiki/%D7%94%D7%95%D7%A8%D7%95%D7%AA_%D7%9E%D7%A9%D7%95%D7%AA%D7%A4%D7%AA">
-                        לעיון בערך המלא
+                    {Contents.info.paragraphs.map((p) => (
+                        <div key={p}>{p}</div>
+                    ))}
+                    <div className={classes.wiki}>{Contents.info.credit}</div>
+                    <a
+                        href={Contents.info.creditLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {Contents.info.creditLink}
                     </a>
                 </div>
             </div>
